597c1095c27f7edd38966b3cb6164a6cb1bb3f1a
Daniil Novoselov <me@gudn.link>
2026-03-25 10:04:02 +0300

    fix typo in kitty

--- home-modules/desktop/kitty.nix
+++ home-modules/desktop/kitty.nix
@@ -75,7 +75,7 @@
 
         "ctrl+equal" = "change_font_size all +2.0";
         "ctrl+plus" = "change_font_size all +2.0";
-        "ctrl+minus" = "change_font_size all +2.0";
+        "ctrl+minus" = "change_font_size all -2.0";
         "ctrl+backspace" = "change_font_size all 0";
       };
     };
